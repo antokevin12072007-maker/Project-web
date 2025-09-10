37035114f6a2f561e0e76cd70d9fda90779947ca
antokevin12072007-maker <antokevin12072007@gmail.com>
2025-09-10 15:11:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const toggle = document.getElementById("theme-toggle");
+const body = document.body;
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark");
+  toggle.checked = true;
+}
+
+// Switch theme
+toggle.addEventListener("change", () => {
+  body.classList.toggle("dark");
+  localStorage.setItem("theme", body.classList.contains("dark") ? "dark" : "light");
+});
